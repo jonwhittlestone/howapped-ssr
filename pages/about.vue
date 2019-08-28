27b54a03e297a5d2c55fea4da30d41e27211e8f9
Jon Whittlestone <dev@howapped.com>
2019-08-28 08:45:23 +0100

    Moar copy

--- pages/about.vue
+++ pages/about.vue
@@ -3,27 +3,42 @@
     <div class="container">
       <h2 class="title is-2">About</h2>
       <p>
-          Hello, I'm Jon and building great user-focused systems is my passion.
+          Hello, I'm Jon and my passion is building great user-centric systems.
       </p>
       <p>
-          I've been lucky enough to be involved in the web since the early challenges of analogue modems, ubiqutious on-premise infrastructure and pre-CSS and we've overcome those challenges, to emerge with distributed systems that are more robust and cheaper to maintain and scale.
+          I've been lucky enough to be involved in the web since the early challenges of analogue modems, ubiqutious on-premise infrastructure and pre-CSS.<br>We've overcome those challenges, to emerge with distributed systems that are more robust and cheaper to maintain and scale.
       </p>
-      <p>As an end-to-end developer, I am able to help your organisation by observing Howapped's three principal features:</p>
+      <p>As an end-to-end developer, I am able to help your organisation by observing Howapped's three principal values:</p>
       
       <br />
 
       <ul>
-        <li>Full-stack Web development</li>
-        <li>Quality</li>
-        <li>Productivity</li>
+        <li><strong>Practice Full-stack Web development</strong></li>
+            <ul>
+                <li>Agency or Entrepeneur, Howapped can help any business flourish with custom apps tailored to the projects' needs</li>
+            </ul>
+        <br>
+        <li><strong>Ensure Quality</strong></li>
+            <ul>
+                <li>Adding automated testing for greater confidence that quality is delivered, on-budget and on-schedule</li>
+            </ul>
+        <br>
+        <li><strong>Foster Productivity</strong></li>
+            <ul>
+                <li>Building and maintaining a happy and productive development environement and team </li>
+            </ul>
+        <br>
       </ul>
 
       <br />
       <h4 class="title is-4">Building to last</h4>
 
-      <p>As a software generalist, I've seen technologies come and go, thrive and flourish and I'm dedicated to architecting with the most appropriate stack for the job.</p>
+      <p>As a generalist, I've <strong>observed</strong> technologies come and go, thrive and flourish and I'm dedicated to <strong>act</strong>, architecting with the most appropriate stack for the job.</p>
     </div>
   </div>
-</template><script>
+</template>
+<script>
 export default { head: { title: "About" } };
 </script>
+<style scoped>
+</style>
